92e48134088607d9e7abe7015d62caaeb2b7e5fb
AntonP2001 <my.antonp2001@gmail.com>
2023-04-30 12:24:02 +0300

    Добавлены св-ва ранга матрицы и самой матрицы

--- CompTestingVar12/MainWindow.xaml.cs
+++ CompTestingVar12/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,8 +20,42 @@ namespace CompTestingVar12
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    /// 
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        #region INPC Realization
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+        #endregion
+
+        #region Свойства
+        private int _matrixRank;
+        public int MatrixRank
+        {
+            get => _matrixRank;
+            set
+            {
+                _matrixRank = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Array[][] _matrix;
+        public Array[][] Matrix
+        {
+            get => Matrix;
+            set
+            {
+                _matrix = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
         public MainWindow()
         {
             InitializeComponent();
